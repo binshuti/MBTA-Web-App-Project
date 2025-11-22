dba66e57aaf366a4b259a227e532ad3105977467
Arjun Masciarelli <arjun@masciarellis.com>
2025-11-22 01:17:16 -0500

    Added next train arrival time and moved css to stylesheets

--- mbta_helper.py
+++ mbta_helper.py
@@ -76,6 +76,7 @@ def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
         "sort": "distance",
         "filter[latitude]": latitude,
         "filter[longitude]": longitude,
+        "filter[location_type]": "1",
         "page[limit]": "1",  # only want the closest stop
     }
     query_string = urllib.parse.urlencode(params)
@@ -83,42 +84,87 @@ def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
 
     data = get_json(url)
     stops = data.get("data", [])
+
     if not stops:
         raise RuntimeError(
             f"No MBTA stops found near lat={latitude}, lon={longitude}"
         )
-
     stop = stops[0]
+    stop_id = stop["id"]
+    print("debug print stop_id:", stop_id)
+    
     attributes = stop.get("attributes", {})
 
     station_name = attributes.get("name", "Unknown stop")
     wheelchair_code = attributes.get("wheelchair_boarding", 0)
     # 0 = unknown, 1 = accessible, 2 = not accessible
     wheelchair_accessible = wheelchair_code == 1
+    return station_name, wheelchair_accessible, stop_id
 
-    return station_name, wheelchair_accessible
+# def get_next_scheduled_train(stop_id: str) -> dict | None:
+#     """
+#     Gets the departure time of the first scheduled train. Can be subbed 
+#     instead of using MBTA's own internal WebAPI
+#     """
+#     params = {
+#         "api_key": MBTA_API_KEY,
+#         "filter[stop]": stop_id,
+#         "sort": "departure_time",
+#         "page[limit]": "1",
+#     }
 
+#     url = f"{MBTA_BASE_URL}schedules?{urllib.parse.urlencode(params)}"
+#     data = get_json(url)
 
-def find_stop_near(place_name: str) -> tuple[str, bool]:
+#     schedules = data.get("data", [])
+#     if not schedules:
+#         return None
+#     return schedules[0]["attributes"]["departure_time"]
+
+def find_stop_near(place_name: str) -> tuple[str, bool, dict | None]:
     """
     Given a place name or address, return the nearest MBTA stop and whether
     it is wheelchair accessible.
     """
     lat, lng = get_lat_lng(place_name)
-    return get_nearest_station(lat, lng)
+    station_name, accessible, stop_id = get_nearest_station(lat, lng)
+    next_train = get_next_train_webapi(stop_id)
+    next_train = next_train['arrival_time']
+    return station_name, accessible, next_train
 
+def get_next_train_webapi(stop_id: str) -> dict | None:
+    """
+    Uses the MBTA's public WebAPI from their website to retrieve
+    the next train at the given stop_id. Has better accuracy than V3-MBTA
+    schedules end point
+    """
+    url = f"https://www.mbta.com/api/stops/{stop_id}/schedules?future_departures=true&last_stop_departures=false"
+    data = get_json(url)
+
+    if not data:
+        return None
+
+    next_trip = data[0]  # the first item is the next departure
+
+    return {
+        "arrival_time": next_trip.get("arrival_time")
+    }
 
 def main():
     """
     Simple manual tests.
     Run: python mbta_helper.py
     """
-    test_place = "Boston Common"
+    test_place = "Brookline High School"
     print("Testing get_lat_lng:")
     print(test_place, "->", get_lat_lng(test_place))
 
     print("\nTesting find_stop_near:")
     print(test_place, "->", find_stop_near(test_place))
+
+    print("\nTesting get_next_scheduled_train:")
+    station, wheelchair_or_not, schedule = find_stop_near(test_place)
+    print("Next train:", schedule)
 
 
 if __name__ == "__main__":
